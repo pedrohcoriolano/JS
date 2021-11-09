c4177b70e1d6e1eab06ad1dc6a7f6b5deb25af9f
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2021-11-09 17:00:25 -0300

    Update javascript.js

--- Desafios/ex001/javascript.js
+++ Desafios/ex001/javascript.js
@@ -7,15 +7,12 @@ function carregar() {
     msg.innerHTML = `Agora são ${agora} horas`
 
     if (agora >= 0 && agora < 12) {
-        //bom dia
         foto.src = 'manha.jpg'
         document.body.style.background = '#e2cd9f'
     } else if (agora >= 12 && agora < 18) {
-        //boa tarde
         foto.src = 'tarde.jpg'
         document.body.style.background = '#b9846f'
     } else {
-        //boa noite
         foto.src = 'noite.jpg'
         document.body.style.background = '#515154'
     }
