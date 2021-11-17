93859eece0a238a36a057bc761199b53715e5d82
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2021-11-17 09:35:12 -0300

    novo ex

---
+++ Práticas/ex016.js
@@ -0,0 +1,10 @@
+/*var c = 1
+while (c <= 500) {
+    console.log(`Essa mensgaem está sendo repetida ${c} vezes`)
+    c++
+}*/
+var c = 1
+do {
+    console.log('Olá mundo!')
+    c++
+} while (c <= 10)
