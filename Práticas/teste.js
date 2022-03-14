dbeee54a0a678dd00fc24785ddd103fc8c012e3f
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-03-14 18:45:11 -0300

    teste

---
+++ Práticas/teste.js
@@ -0,0 +1,12 @@
+let nome = "d"
+
+switch(nome){
+    case "p":
+        console.log("pedro")
+        break
+    case "d":
+        console.log("daniel")
+        break
+    default:
+        console.error("valor invalido");
+}
